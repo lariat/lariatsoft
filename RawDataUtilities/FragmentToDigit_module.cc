4700f3993d52add6d9f58a0f0f814db44b726f8e
Johnny Ho <johnnyho@fnal.gov>
2015-03-02 14:27:54 -0600

    Added ability to create AuxDetDigits for the WUT and MWPC TDCs.

--- RawDataUtilities/FragmentToDigit_module.cc
+++ RawDataUtilities/FragmentToDigit_module.cc
@@ -13,8 +13,24 @@
 // [x] Add CAEN V1751 board 1
 // [x] Add CAEN V1751 board 2
 // [x] Add channels 32 to 64 of CAEN V1740 board 8
-// [ ] Add WUT
-// [ ] Add MWPCs
+// [x] Add WUT
+// [x] Add MWPCs
+//
+// [ ] Test with DigitReader module
+//     NOTE: Testing may be difficult to do because the files
+//           in /lariat/data/users/willhf/140925 do not have
+//           the spill/subrun number in them. Events with the
+//           same run and subrun numbers are seemingly treated
+//           as duplicates (and the second instance is
+//           ignored?).
+//           Might be a problem during the LArIATDAQ->ArtDAQ
+//           format conversion of the old files? New files
+//           written by ArtDAQ do not have to same problem. In
+//           any case, the latest PID/TOF plots CANNOT be
+//           reproduced with AuxDetDigits until this is fixed.
+//
+// [ ] Add helpful comments throughout code. This may never be
+//     checked off.
 //////////////////////////////////////////////////////////////
 
 #include "art/Framework/Core/EDProducer.h"
@@ -32,15 +48,16 @@
 
 #include "artdaq-core/Data/Fragment.hh"
 #include "LariatFragment.h"
-//#include "WUTFragment.h"
+#include "WUTFragment.h"
 #include "CAENFragment.h"
-//#include "TDCFragment.h"
+#include "TDCFragment.h"
 
 #include "RawData/AuxDetDigit.h"
 
 #include "TTree.h"
 
 #include <memory>
+#include <functional>
 #include <vector>
 #include <string>
 
@@ -49,6 +66,8 @@ enum {
   V1740_N_SAMPLES = 1536,
   V1751_N_CHANNELS = 8,
   V1751_N_SAMPLES = 1792,
+  WUT_N_TDC_CHANNELS = 16,
+  WUT_MAX_HITS = 128,
 };
 
 class FragmentToDigit;
@@ -74,11 +93,28 @@ public:
 
 private:
 
-  std::string           fRawFragmentLabel;    ///< label for module producing artdaq fragments
-  std::string  		    fRawFragmentInstance; ///< instance label for artdaq fragments        
-  std::string           fCaenV1740Board8Label;
-  std::string           fCaenV1751Board1Label;
-  std::string           fCaenV1751Board2Label;
+  std::string fRawFragmentLabel;     ///< label for module producing artdaq fragments
+  std::string fRawFragmentInstance;  ///< instance label for artdaq fragments        
+  std::string fCaenV1740Board8Label;
+  std::string fCaenV1751Board1Label;
+  std::string fCaenV1751Board2Label;
+  std::string fWutLabel;
+  std::string fMwpcTdc01Label;
+  std::string fMwpcTdc02Label;
+  std::string fMwpcTdc03Label;
+  std::string fMwpcTdc04Label;
+  std::string fMwpcTdc05Label;
+  std::string fMwpcTdc06Label;
+  std::string fMwpcTdc07Label;
+  std::string fMwpcTdc08Label;
+  std::string fMwpcTdc09Label;
+  std::string fMwpcTdc10Label;
+  std::string fMwpcTdc11Label;
+  std::string fMwpcTdc12Label;
+  std::string fMwpcTdc13Label;
+  std::string fMwpcTdc14Label;
+  std::string fMwpcTdc15Label;
+  std::string fMwpcTdc16Label;
 
 };
 
@@ -92,12 +128,29 @@ FragmentToDigit::FragmentToDigit(fhicl::ParameterSet const & p)
   produces< std::vector<raw::AuxDetDigit> >(fCaenV1740Board8Label);
   produces< std::vector<raw::AuxDetDigit> >(fCaenV1751Board1Label);
   produces< std::vector<raw::AuxDetDigit> >(fCaenV1751Board2Label);
+  produces< std::vector<raw::AuxDetDigit> >(fWutLabel);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc01Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc02Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc03Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc04Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc05Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc06Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc07Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc08Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc09Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc10Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc11Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc12Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc13Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc14Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc15Label);
+  produces< std::vector<raw::AuxDetDigit> >(fMwpcTdc16Label);
 }
 
 //------------------------------------------------------------------------------
 void FragmentToDigit::reconfigure(fhicl::ParameterSet const & p)
 {
-  fRawFragmentLabel    = p.get< std::string >("RawFragmentLabel", "daq");
+  fRawFragmentLabel = p.get< std::string >("RawFragmentLabel", "daq");
   fRawFragmentInstance = p.get< std::string >("RawFragmentInstance", "SPILL");
   fCaenV1740Board8Label = p.get< std::string >("CaenV1740Board8Label",
                                                "CaenV1740Board8");
@@ -105,6 +158,23 @@ void FragmentToDigit::reconfigure(fhicl::ParameterSet const & p)
                                                "CaenV1751Board1");
   fCaenV1751Board2Label = p.get< std::string >("CaenV1751Board2Label",
                                                "CaenV1751Board2");
+  fWutLabel = p.get< std::string >("WutLabel", "Wut");
+  fMwpcTdc01Label = p.get< std::string >("MwpcTdc01Label", "MwpcTdc01");
+  fMwpcTdc02Label = p.get< std::string >("MwpcTdc02Label", "MwpcTdc02");
+  fMwpcTdc03Label = p.get< std::string >("MwpcTdc03Label", "MwpcTdc03");
+  fMwpcTdc04Label = p.get< std::string >("MwpcTdc04Label", "MwpcTdc04");
+  fMwpcTdc05Label = p.get< std::string >("MwpcTdc05Label", "MwpcTdc05");
+  fMwpcTdc06Label = p.get< std::string >("MwpcTdc06Label", "MwpcTdc06");
+  fMwpcTdc07Label = p.get< std::string >("MwpcTdc07Label", "MwpcTdc07");
+  fMwpcTdc08Label = p.get< std::string >("MwpcTdc08Label", "MwpcTdc08");
+  fMwpcTdc09Label = p.get< std::string >("MwpcTdc09Label", "MwpcTdc09");
+  fMwpcTdc10Label = p.get< std::string >("MwpcTdc10Label", "MwpcTdc10");
+  fMwpcTdc11Label = p.get< std::string >("MwpcTdc11Label", "MwpcTdc11");
+  fMwpcTdc12Label = p.get< std::string >("MwpcTdc12Label", "MwpcTdc12");
+  fMwpcTdc13Label = p.get< std::string >("MwpcTdc13Label", "MwpcTdc13");
+  fMwpcTdc14Label = p.get< std::string >("MwpcTdc14Label", "MwpcTdc14");
+  fMwpcTdc15Label = p.get< std::string >("MwpcTdc15Label", "MwpcTdc15");
+  fMwpcTdc16Label = p.get< std::string >("MwpcTdc16Label", "MwpcTdc16");
 }
 
 //------------------------------------------------------------------------------
@@ -157,6 +227,57 @@ void FragmentToDigit::produce(art::Event & evt)
       (new std::vector<raw::AuxDetDigit>);
   std::unique_ptr< std::vector<raw::AuxDetDigit> > caenV1751Board2Vec
       (new std::vector<raw::AuxDetDigit>);
+
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > wutVec
+      (new std::vector<raw::AuxDetDigit>);
+
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc01Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc02Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc03Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc04Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc05Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc06Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc07Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc08Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc09Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc10Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc11Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc12Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc13Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc14Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc15Vec
+      (new std::vector<raw::AuxDetDigit>);
+  std::unique_ptr< std::vector<raw::AuxDetDigit> > mwpcTdc16Vec
+      (new std::vector<raw::AuxDetDigit>);
+
+  // the following line is way too long
+  std::vector< std::reference_wrapper< std::unique_ptr< std::vector<raw::AuxDetDigit> > > > mwpcTdcVecs = {
+      mwpcTdc01Vec, mwpcTdc02Vec, mwpcTdc03Vec, mwpcTdc04Vec,
+      mwpcTdc05Vec, mwpcTdc06Vec, mwpcTdc07Vec, mwpcTdc08Vec,
+      mwpcTdc09Vec, mwpcTdc10Vec, mwpcTdc11Vec, mwpcTdc12Vec,
+      mwpcTdc13Vec, mwpcTdc14Vec, mwpcTdc15Vec, mwpcTdc16Vec
+      };
+
+  std::string mwpcTdcLabels[16] = {
+      fMwpcTdc01Label, fMwpcTdc02Label, fMwpcTdc03Label, fMwpcTdc04Label,
+      fMwpcTdc05Label, fMwpcTdc06Label, fMwpcTdc07Label, fMwpcTdc08Label,
+      fMwpcTdc09Label, fMwpcTdc10Label, fMwpcTdc11Label, fMwpcTdc12Label,
+      fMwpcTdc13Label, fMwpcTdc14Label, fMwpcTdc15Label, fMwpcTdc16Label
+      };
 
   if ( !fragments.isValid() )
       throw cet::exception("FragmentToDigit")
@@ -248,9 +369,124 @@ void FragmentToDigit::produce(art::Event & evt)
 
   }
 
+  const int numberWutFrags = data->wutFrags.size();
+  std::cout << "Found " << numberWutFrags << " WUT fragments" << std::endl;
+
+  if (numberWutFrags > 0) {
+    std::cout << "Looking at WUT fragments..." << std::endl;
+  }
+
+  for (int i = 0; i < numberWutFrags; ++i) {
+    WUTFragment & wutFrag = data->wutFrags[i];
+    wutFrag.print();
+    uint32_t numberHits = wutFrag.header.nHits;
+    std::vector<WUTFragment::WutHit> & hits = wutFrag.hits;
+
+    std::vector< std::vector<short> > hitsInChannel;
+
+    hitsInChannel.resize(WUT_N_TDC_CHANNELS);
+    for (size_t channel = 0; channel < WUT_N_TDC_CHANNELS;
+         ++channel) {
+      hitsInChannel[channel].reserve(WUT_MAX_HITS);
+    }
+
+    uint32_t timeHeader = wutFrag.header.timeHeader;
+
+    for (size_t j = 0; j < numberHits; ++j) {
+      WUTFragment::WutHit & hit = hits[j];
+      hitsInChannel[size_t (hit.channel)].push_back(short (hit.timeBin));
+    }
+
+    for (size_t channel = 0; channel < WUT_N_TDC_CHANNELS;
+         ++channel) {
+
+      wutVec->push_back(
+          raw::AuxDetDigit(
+              static_cast <unsigned short> (channel),
+              hitsInChannel[channel],
+              fWutLabel,
+              static_cast <unsigned long long> (timeHeader)
+              )
+          );
+
+    }
+  }
+
+  const int numberTdcFrags = data->tdcFrags.size();
+  std::cout << "Found " << numberTdcFrags << " TDC fragments" << std::endl;
+
+  if (numberTdcFrags > 0) {
+    std::cout << "Looking at TDC fragments..." << std::endl;
+  }
+
+  for (int i = 0; i < numberTdcFrags; ++i) {
+
+    TDCFragment & tdcFrag = data->tdcFrags[i];
+
+    std::vector< std::vector<TDCFragment::TdcEventData> > &
+        tdcEvents = tdcFrag.tdcEvents;
+
+    //std::cout << "tdcEvents.size(): " << tdcEvents.size() << std::endl;
+    //tdcFrag.print();
+
+    for (size_t j = 0; j < tdcEvents.size(); ++j) {
+
+      if (tdcFrag.controllerHeader.nTDCs != tdcEvents[j].size()) {
+        std::cout << "*** Fatal nTDCs mismatch: " << tdcEvents[j].size()
+                  << " != " << tdcFrag.controllerHeader.nTDCs << std::endl;
+      }
+
+      for (size_t tdc_index = 0; tdc_index < TDCFragment::MAX_TDCS;
+           ++tdc_index) {
+        TDCFragment::TdcEventData tdcEventData = tdcEvents[j].at(tdc_index);
+
+        std::vector< std::vector<short> > hitsInChannel;
+
+        hitsInChannel.resize(TDCFragment::N_CHANNELS);
+        for (size_t channel = 0; channel < TDCFragment::N_CHANNELS;
+             ++channel) {
+          hitsInChannel[channel].reserve(TDCFragment::MAX_HITS);
+        }
+
+        //uint32_t triggerCounter = tdcEventData.tdcEventHeader.triggerCounter;
+        //uint16_t controllerTimeStamp =
+        //    tdcEventData.tdcEventHeader.controllerTimeStamp;
+
+        uint32_t tdcTimeStamp = tdcEventData.tdcEventHeader.tdcTimeStamp;
+
+        for (size_t hit_index = 0; hit_index < tdcEventData.tdcHits.size();
+             ++hit_index) {
+          TDCFragment::TdcHit & hit = tdcEventData.tdcHits[hit_index];
+          hitsInChannel[size_t (hit.channel)].push_back(short (hit.timeBin));
+        }
+
+        for (size_t channel = 0; channel < TDCFragment::N_CHANNELS;
+             ++channel) {
+
+          mwpcTdcVecs[tdc_index].get()->push_back(
+              raw::AuxDetDigit(
+                  static_cast <unsigned short> (channel),
+                  hitsInChannel[channel],
+                  mwpcTdcLabels[tdc_index],
+                  static_cast <unsigned long long> (tdcTimeStamp)
+                  )
+              );
+
+        }
+
+      }
+    }
+  }
+
   evt.put(std::move(caenV1740Board8Vec), fCaenV1740Board8Label);
   evt.put(std::move(caenV1751Board1Vec), fCaenV1751Board1Label);
   evt.put(std::move(caenV1751Board2Vec), fCaenV1751Board2Label);
+
+  evt.put(std::move(wutVec), fWutLabel);
+
+  for (size_t i = 0; i < TDCFragment::MAX_TDCS; ++i) {
+    evt.put(std::move(mwpcTdcVecs[i].get()), mwpcTdcLabels[i]);
+  }
 
   return;  
 }
