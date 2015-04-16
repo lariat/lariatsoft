e23676a9ec7d987cac2be260011ce393edd27013
Will Flanagan <willhf@lariatgpvm01.fnal.gov>
2015-04-16 17:48:42 -0500

    First git version of the beamline simulation.

---
+++ BeamlineSim/jobdir_SurveyedGeom/MergeTrees.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+# Author: Jason St. John
+# A very brief skeleton of a script to read in a file of TTrees, all assumed to be part of
+# the same simulated particle interactions, and save them in a single new tree, indexed by
+# the unique combinations of their Track and Event numbers. 
+#
+# Usage:
+# ./MergeTrees.py <options> results.root 
+
+import ROOT
+import sys
+import optparse
+import commands
+import os
+import glob
+from decimal import Decimal
+import re
+from array import *
+import math
+from ctypes import *
+import random
+
+parser = optparse.OptionParser("usage: %prog [options]<input file.ROOT> \n")
+parser.add_option ('--o', dest='outfile', type='string',
+                   default = 'MergedTree_test.root',
+                   help="Output filename (ends .root).  This option is ignored.")
+parser.add_option ('-T', dest='starterTree', type='string',
+                   default = 'BigDisk',
+                   help="The one TTree whose tracks will be iterated over. Effectively requires tracks present here.")
+parser.add_option ('-l', dest='keepitlocal', action="store_true", default=False,
+                   help="Keep the output file in the same directory as the input file.")
+
+
+options, args = parser.parse_args()
+outfile = options.outfile
+debug = False
+starterTree = options.starterTree
+keepitlocal   = options.keepitlocal
+infile = args[0]
+
+################################
+# Timing offset generator
+################################
+BatchesPerOrbit = 7
+BucketsPerBatch = 84
+BucketsPerOrbit = BatchesPerOrbit * BucketsPerBatch
+
+bucketcenterspacing = float("18.8e-9")
+bucketwidth = float("2.2e-9")
+batchlength = bucketcenterspacing * float(BucketsPerBatch)
+orbitlength = batchlength * float(BatchesPerOrbit)
+spillduration = 4.2
+OrbitsInSpill = spillduration / orbitlength
+filledbatches = (1,2,3,4,5,6) # (out of BatchesPerOrbit)
+
+def RandomOffsetSeconds ():
+    BucketInBatch = random.randint(1,BucketsPerBatch-1)
+    BatchInOrbit = random.choice(filledbatches)
+    OrbitInSpill = random.randint(0,int(OrbitsInSpill))
+    
+    offset = random.gauss(0,bucketwidth)
+    offset += bucketcenterspacing * float(BucketInBatch)
+    offset += batchlength * BatchInOrbit
+    offset += orbitlength * OrbitInSpill
+    return offset
+
+
+##############################################
+# Check files exist
+##############################################
+if not os.path.isfile(infile) :
+    print "Cannot find %s.  Exiting." % infile
+    sys.exit()
+
+infiles = glob.glob(infile)
+base = ""  ## The path to the file.
+for file in infiles:
+    base = os.path.splitext(file)[0]
+    #print file, base
+
+
+infilename = infile
+infile = ROOT.TFile(infile)
+################################
+# Set up ROOT
+################################
+
+#  Do ROOT initialization stuff here so that
+#  my --help gets reported instead of ROOT --help.
+
+ROOT.gROOT.SetBatch(0)
+#ROOT.gStyle.SetOptStat(0)
+#ROOT.gStyle.SetPalette(1)
+#ROOT.gStyle.SetNdivisions(405,"x")
+
+#####################################
+# Declare & fill dictionaries
+#####################################
+INtuples = {}
+
+#########################################
+# Get into the file and see what's there
+#########################################
+infile.cd()
+ROOT.gDirectory.cd()
+abspath = base+".root:/VirtualDetector/"
+print "|",abspath,"|"
+ROOT.gDirectory.cd(abspath)
+ROOT.gDirectory.ls()
+
+dumtuple = ROOT.TNtuple() # Just need an instance of this class, it seems.
+
+if debug: print "Looping over input file contents, getting trees."
+for key in ROOT.gDirectory.GetListOfKeys():
+    if dumtuple.Class() == key.ReadObj().Class():
+        if debug: print key.GetName(),
+        if debug: print key.ReadObj().ClassName()
+        INtuples[key.GetName()] =  key.ReadObj()
+
+for name, tuple in INtuples.iteritems():
+    if debug: print "Building an index for ",name,"...",
+    tuple.BuildIndex("EventID","TrackID")
+    if debug: print "Done."
+
+vars = ('x','y','z','t','Px','Py','Pz','PDGid','ParentID','EventID','TrackID')
+WCs = ('Det1', 'Det2', 'Det3', 'Det4')
+Scints = ('TOFus', 'Halo', 'HaloHole', 'TOFdsHorz', 'TiWindow','BigDisk')
+Punch  =  ('PunchUL', 'PunchLL', 'PunchUR', 'PunchLR')
+
+### One dictionary to rule them all. ##
+## Unfortunately, ROOT won't process a single line defining a single struct for all these; too long.  We break it into two, here.
+detsysts = {} 
+detsysts['WCs'] = WCs
+detsysts['Scints'] = Scints
+detsysts['Punch'] = Punch
+
+
+#Invent some types of struct for holding stuff from the tree
+for systname,syst in detsysts.iteritems():
+    linetoprocess = "struct "+systname+"stuff { Int_t EventID; Int_t TrackID; "
+    ## Be sure to do the ints before the floats ##
+    for var in ('EventID', 'TrackID'):
+        for det in syst:
+            linetoprocess += "Int_t " + var + det + "; "
+    for var in vars:
+        if var == "EventID" or var == "TrackID": continue  #Don't re-do these ints!
+        for det in syst:
+            linetoprocess += "Float_t " + var + det + "; "
+    linetoprocess += "};"
+    if debug: print linetoprocess,"\n\n"
+    ROOT.gROOT.ProcessLine(linetoprocess)
+    
+#Make one of each of them to use
+structs = {}
+for systname in detsysts.keys():
+    structs[systname] = eval("ROOT."+systname+"stuff()")
+
+#Hook the variables to their branches, so they take on the right values when an entry is fetched.
+for systname,syst in detsysts.iteritems():
+    if debug: print "\n Syst:",systname
+    for var in vars:
+        if debug: print "    now on :",var
+        for det in syst:
+            if debug: print "       det:",det
+            tuple = INtuples[det]
+            tuple.SetBranchAddress(var, ROOT.AddressOf(structs[systname],var+det))
+
+#### The Output: A file with a tree in it. ####
+if keepitlocal:
+    outfile = ROOT.TFile("MergedAt"+starterTree+infilename,"RECREATE")
+else:
+    outfile = ROOT.TFile("/lariat/data/users/stjohn/MergedAt"+starterTree+infilename,"RECREATE")
+
+newTree = ROOT.TTree("EventTree","EventTree")
+## Two important branches to include:
+EventID_p = array( 'i', [ 0 ] )
+TrackID_p = array( 'i', [ 0 ] )
+newTree.Branch("EventID",EventID_p,"EventID/I")
+newTree.Branch("TrackID",TrackID_p,"TrackID/I")
+## ...and all the others we want:
+pointers = {}
+if debug: print "                 Filling Pointers:"
+for syst in detsysts.values():
+    for det in syst:
+        name = 'TrackPresent'+det
+        if debug: print "                              ",name
+        pointers[name] = array( 'B', [ 0 ] )
+        newTree.Branch(name,pointers[name],name+"/O")
+for var in vars:
+    if var == "EventID" or var == "TrackID": continue  # Only one unique combo of these, already done above.
+    for syst in detsysts.values():
+        for det in syst:
+            name = var+det
+            if debug: print "                              ",name
+            pointers[name] = array( 'd', [ 0 ] )
+            newTree.Branch(name,pointers[name],name+"/d")
+
+outfile.cd()
+
+## How to remove the trigger-like requirement?
+## Maybe we loop over everything which encountered the Halo paddle (including the HaloHole volume),
+## and also add whatever additional entries can be found in the first WireChamber.
+
+trackcount = 0
+for ds_track in INtuples[starterTree]:
+    trackcount += 1
+    ## Unique identifiers for this track:
+    (event, track) = (int(ds_track.EventID), int(ds_track.TrackID))
+    # Development purposes: (Abbreviated run)
+    #if trackcount > 1000: break
+
+    ## What I hate about this is that the newTree looks completely uninvolved.
+    ## Of course, it's tied in above, at newTree.Branch(name,pointers[name],name+"/f")
+    EventID_p[0] = event
+    TrackID_p[0] = track
+
+    #Momentum cut? if ds_track.Pz < 1000. : continue
+    for tuplename,tuple in INtuples.iteritems():
+        for syst in detsysts.values():
+            if tuplename not in syst: continue
+            EntryNum = tuple.GetEntryWithIndex(event,track)
+
+            if EntryNum == -1: #Track Not Found
+                pointers['TrackPresent'+tuplename][0] = False
+            else:
+                pointers['TrackPresent'+tuplename][0] = True
+            for var in vars:
+                if var == 'EventID' or var == 'TrackID': continue
+                ## Pointers are named like 'PzWC4' etc..
+                vardet = var + tuplename
+                if EntryNum == -1: #Track Not Found
+                    pointers[vardet][0] = -123456789  ##Dummy Value
+                    if debug: print "(",event,",",track,"): ",vardet,"       =-1"
+                else: # Track found.
+                    for systname,syst in detsysts.iteritems():
+                        if tuplename in syst:
+                            if debug: print "Filling ",vardet," in ",systname
+                            if var == 't':  # Convert times to seconds & add an offset mimicking spill time profile
+                                random.seed(event) #Same random offset for each event.
+                                pointers[vardet][0] = getattr(structs[systname],vardet)*1e-9 + RandomOffsetSeconds()
+                            else:
+                                pointers[vardet][0] = getattr(structs[systname],vardet)
+                                break #tuplename will only be in one syst, not more. q
+    newTree.Fill()
+print trackcount, "  total tracks in ",starterTree
+outfile.cd()
+newTree.Write()
+outfile.Close()
+#-------------- /Final Beam --------------------#
+
+infile.Close()
+if not keepitlocal: commands.getoutput("ln -s /lariat/data/users/stjohn/MergedAt"+starterTree+infilename+" MergedAt"+starterTree+infilename)
+print "Here ya go: ",
+commands.getoutput("ls -ltra MergedAt"+starterTree+infilename)
