ddf6c7d96357ca0fee8499005de128f400bd152a
Jonathan Asaadi <jaasaadi@syr.edu>
2015-05-29 12:24:23 -0500

    Fixing bugs in skeleton code

--- LArIATRecoModule/MyNewMod_module.cc
+++ LArIATRecoModule/MyNewMod_module.cc
@@ -62,6 +62,7 @@ private:
   
   std::string fTriggerUtility; //<---Label for the module producing the triggers
   TH1F* fWC1Size;
+  TH1F* fWC1XPlaneADC;
   
   // Declare member data here.
 
@@ -79,10 +80,19 @@ void lrm::MyNewMod::produce(art::Event & e)
 {
   // Implementation of required member function here.
   
+  // ---- Example of how to make a collection of new object you are putting on the event ---
+  //std::unique_ptr<std::vector<recob::WireChamberTrack> > WCtrackcol(new std::vector<recob::WireChamberTrack>);
+  
+  
+  // ---- Example of how to make an association between your new object and the trigger object -----
+  //std::unique_ptr<art::Assns<raw::Trigger, recob::WireChamberTrack> > TrigWCtrackAssn(new art::Assns<raw::Trigger, recob::WireChamberTrack>);
   
   // ###########################################
   // ### Grab the trigger data utility (tdu) ###
   // ###########################################
+  
+  // ### This is a crap way to pass a string.....FIX ME!!!! ####
+  fTriggerUtility = "FragmentToDigit";
   rdu::TriggerDigitUtility tdu(e, fTriggerUtility);
     
     
@@ -91,15 +101,48 @@ void lrm::MyNewMod::produce(art::Event & e)
   // ##############################
   for(size_t trig = 0; trig < tdu.NTriggers(); ++trig)
      {
+     
+     // ### Global Trigger that you will use to make associations ###
+     //art::Ptr<raw::Trigger> trigger = tdu.EventTriggersPtr()[trig];
+     
+     // ### Getting a vector of Wire Chamber 1 Digits ###
      std::vector<const raw::AuxDetDigit*> WireChamber1Digi = tdu.TriggerMWPC1Digits(trig);
      
      std::cout<<"Size of WC1Digi = "<<WireChamber1Digi.size()<<std::endl;
      fWC1Size->Fill(WireChamber1Digi.size());
      
+     // ### Looping over Wire Chamber #1's Digits ###
      for(size_t wc1 = 0; wc1 < WireChamber1Digi.size(); ++wc1)
         {
-	std::cout<<"WireChamber1Digi[wc1] = "<<WireChamber1Digi[wc1]<<std::endl;
+	//std::cout<<"WireChamber1Digi Channel Number = "<<WireChamber1Digi.at(wc1)->Channel()<<std::endl;
+	///std::cout<<"WireChamber1Digi TDC = "<<WireChamber1Digi.at(wc1)->NADC()<<std::endl;
 	
+	// ### Looping over all the TDC hits (annoyingly called ADC's) ###
+	for (size_t i =0; i < WireChamber1Digi.at(wc1)->NADC(); ++i)
+	   {
+	
+	   // ### Skipping any TDC hits (which returns the time tick) that is zero ###
+	   if(WireChamber1Digi.at(wc1)->ADC(i) == 0){continue;}
+	    
+	      
+	   //### Clustering over X Plane ###
+	   if(WireChamber1Digi.at(wc1)->Channel() < 128) //<---(Channels 0 - 127 are the X Plane)
+	      {
+	      // Get wire and time pairs, cluster with dBScan (or whatever), save the good hits
+	      
+	      fWC1XPlaneADC->Fill(WireChamber1Digi.at(wc1)->ADC(i));
+	      }//<---End X Plane
+	   
+	   // ### Clustering over the Y Plane ###
+	   if(WireChamber1Digi.at(wc1)->Channel() > 127) //<---(Channels 128 - 255 are the Y Plane)
+	      {
+	      
+	       // Get wire and time pairs, cluster with dBScan (or whatever), save the good hits
+	      
+	      }//<---End Y Plane
+	   
+	   
+	   }// <--- End i loop
 	
 	}//<--End wc1
      
@@ -116,7 +159,7 @@ void lrm::MyNewMod::beginJob()
   
   art::ServiceHandle<art::TFileService> tfs;
   fWC1Size = tfs->make<TH1F>("WC1Size","WC1Size", 100, 0, 100);
-  
+  fWC1XPlaneADC  = tfs->make<TH1F>("WC1XPlaneADC", "WC1ADC", 1024, 0, 1024);
 }
 
 void lrm::MyNewMod::beginRun(art::Run & r)
@@ -147,6 +190,12 @@ void lrm::MyNewMod::endSubRun(art::SubRun & sr)
 
 void lrm::MyNewMod::reconfigure(fhicl::ParameterSet const & p)
 {
+
+   /// Here lies some pseudo-code for making objects and their associations 
+   
+   //produces< std::vector<recob::WireChamberTrack>>(); (DON'T USE ME EXPLICITLY....FOR ILLUSTRATION ONLY)
+   //produces< art::Assns<raw::Trigger, recob::WireChamberTrack>>(); (DON'T USE ME EXPLICITLY....FOR ILLUSTRATION ONLY)
+   
    // implementing the ability to pass the name of the TriggerUtililty 
    fTriggerUtility   = p.get< std::string >("TriggerUtility", "FragmentToDigit");
 
