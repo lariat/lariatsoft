72fc16ca39376626068fecddb859c79d94d1e9d4
dlouis95 <dansmith@bu.edu>
2015-06-11 20:50:46 -0500

    Added comments

--- LArIATRecoModule/TimeOfFlight_module.cc
+++ LArIATRecoModule/TimeOfFlight_module.cc
@@ -1,18 +1,22 @@
-////////////////////////////////////////////////////////////////////////
-// Class:       TimeOfFlight
-// Module Type: producer
-// File:        TimeOfFlight_module.cc
-// This is just to change the time stamp of this file
-// Generated at Fri May 29 10:13:48 2015 by Daniel Smith using artmod
-// from cetpkgsupport v1_08_05.
-////////////////////////////////////////////////////////////////////////
-// TO DO:
-////////////////////////////////////////////////////////////////////////
-// [X] Add TOF calculation
-// [X] Add time stamp relative to trigger (? triggerS ?)
-// [ ] Add time stamp to trigger (? triggerS ?) association
-// [ ] Add .root appropriate for producers
-////////////////////////////////////////////////////////////////////////
+/*
+
+LArIATSoft 
+
+Time Of Flight producer module
+
+Module: TimeOfFlight_module.cc 
+FHiCL: TimeOfFlight.fcl
+Header: LArIATDataProducts/TOF.h
+Class: LArIATDataProducts/TOF.cxx
+Dictionary: LArIATDataProducts/classes.h and classes_def.xml
+
+Authores:
+Elena Gramellini - elena.gramellini@yale.edu
+Irene Nutini     - irene.nutini@stud.unifi.it
+Daniel Smith     - dsmith@fnal.gov
+
+*/
+
 
 #include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/ModuleMacros.h"
@@ -33,10 +37,10 @@
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art/Framework/Services/Optional/TFileDirectory.h"
 #include "Utilities/AssociationUtil.h"
-// ### LArIAT Things ###
+
+// LArIAT Things
 #include "RawDataUtilities/TriggerDigitUtility.h"
 #include "RawData/TriggerData.h"
-
 #include "LArIATDataProducts/TOF.h"
 
 namespace lrm {
@@ -52,13 +56,11 @@ public:
   TimeOfFlight & operator = (TimeOfFlight const &) = delete;
   TimeOfFlight & operator = (TimeOfFlight &&) = delete;
 
-  // Required functions.
   void produce(art::Event & e) override;
 
   std::vector<short> find_hits(std::vector<short> wv);
   std::vector<short> match_hits(std::vector<short> hits1, std::vector<short> hits2);
 
-  // Selected optional functions.
   void beginJob() override;
   void beginRun(art::Run & r) override;
   void beginSubRun(art::SubRun & sr) override;
@@ -72,9 +74,11 @@ public:
   void respondToOpenOutputFiles(art::FileBlock const & fb) override;
 
 private:
-  
-  std::string fTriggerUtility; //<---Label for the module producing the triggers
 
+  // Label for the module producing the triggers
+  std::string fTriggerUtility; 
+
+  // ROOT historgrams for debugging purposes 
   TH1F* tof_counts;
   TH1F* ustof_histo;
   TH1F* timestamp_histo;
@@ -83,60 +87,50 @@ private:
 
 
 lrm::TimeOfFlight::TimeOfFlight(fhicl::ParameterSet const & p)
-// :
-// Initialize member data here.
 {
+
+  // Configures the ROOT histograms and the 
   this->reconfigure(p);  
 
-  // Call appropriate produces<>() functions here.
-
+  // Produces the LArSoft object to be ultimately outputted
   produces<std::vector<ldp::TOF> >();
   produces<art::Assns<raw::Trigger, ldp::TOF> >();
-  //  produces<std::vector<short> >();
 
 }
 
 void lrm::TimeOfFlight::produce(art::Event & e)
 {
-  // I don't think the following line is necessary // Elena's addition
-  //  fTriggerUtility = "FragmentToDigit"; 
-
+ 
+  // Setting up to begin looping over the Triggers in the inputted ROOT file
   rdu::TriggerDigitUtility tdu(e, fTriggerUtility);    
-
-  std::unique_ptr<art::Assns<raw::Trigger, ldp::TOF> > TriggerTOFAssn(new art::Assns<raw::Trigger, ldp::TOF>);
-  
-  std::unique_ptr<std::vector<ldp::TOF> > TOFCol(new std::vector<ldp::TOF> );
-
-  //  std::unique_ptr<std::vector<short> > my_tof_ptr (new std::vector<short>);
-
   art::PtrVector<raw::Trigger> const& EventTriggersPtr = tdu.EventTriggersPtr();
 
-  // Loops over the triggers
-  for(size_t trig = 0; trig < tdu.NTriggers(); ++trig) {
-    
-    //    std::cout<<" fTriggerTime "<<int(triggerVect[trig]->TriggerTime()) << std::endl;
-    //    std::cout<< "fBeamGateTime " << int(triggerVect[trig]->BeamGateTime()) << std::endl;
-    //    std::cout<<"TTT " << ust_wv.at(0)->TimeStamp() << std::endl;
+  // Associating the trigger and the TOF to be used in outputting the information to a ROOT file
+  std::unique_ptr<art::Assns<raw::Trigger, ldp::TOF> > TriggerTOFAssn(new art::Assns<raw::Trigger, ldp::TOF>);
+  
+  // Creating the object for the TOF, the object that will be outputted
+  std::unique_ptr<std::vector<ldp::TOF> > TOFCol(new std::vector<ldp::TOF> );
 
-    // Getting a new trigger
+  // Loop over the triggers
+  for(size_t trig = 0; trig < tdu.NTriggers(); ++trig) {
+
+    // Getting a the current trigger
     art::Ptr<raw::Trigger> theTrigger = (EventTriggersPtr.at(trig));
 
-    // Gets the digits for the upstream and downstream paddles
+    // Retrieve the digits for the upstream and downstream paddles
     std::vector<const raw::AuxDetDigit*> ust_wv = tdu.TriggerUpStreamTOFDigits(trig);
     std::vector<const raw::AuxDetDigit*> dst_wv = tdu.TriggerDownStreamTOFDigits(trig);
 
-    // Tests if the event has 2 PMTs. Sometimes an event has 0 PMTs, don't know why
+    // Tests if the event has 2 PMTs, the amount needed for analysis
     if(ust_wv.size() == 2) { 
       
       // Variables of our object
       std::vector<short> tof;
-      std::vector<long> timeToSpillDst;
+      std::vector<long> timeStampDst;
 
-      // Converts the digits into vectors - might be the wrong approach for this
-      std::vector<short> ust_v0;
-      std::vector<short> ust_v1;
-      std::vector<short> dst_v0;
-      std::vector<short> dst_v1;
+      // Converts the digits into vectors to pass into the functions
+      //   might be the wrong approach for this
+      std::vector<short> ust_v0, ust_v1, dst_v0, dst_v1;
       for(size_t i = 1; i < ust_wv.at(0)->NADC(); ++i) { 
       	ust_v0.insert(ust_v0.end(), ust_wv.at(0)->ADC(i));
       	ust_v1.insert(ust_v1.end(), ust_wv.at(1)->ADC(i));
@@ -144,6 +138,7 @@ void lrm::TimeOfFlight::produce(art::Event & e)
       	dst_v1.insert(dst_v1.end(), dst_wv.at(1)->ADC(i));
       }
     
+      // Calls the hit finders for each waveform and then matches the hits, using functions
       std::vector<short> ustof1_hits = find_hits(ust_v0);
       std::vector<short> ustof2_hits = find_hits(ust_v1);
 
@@ -154,6 +149,8 @@ void lrm::TimeOfFlight::produce(art::Event & e)
 
       std::vector<short> dstof_hits = match_hits(dstof1_hits, dstof2_hits);
    
+      // Loops over each of the found matched hits
+      // This compares each found ust hit with each found dst hit
       for(size_t dst_hit = 0; dst_hit < dstof_hits.size(); ++dst_hit) {
 	for(size_t ust_hit = 0; ust_hit < ustof_hits.size(); ++ust_hit) {
 	  
@@ -163,81 +160,88 @@ void lrm::TimeOfFlight::produce(art::Event & e)
 	  // Continues only if the TOF is in an expected range
 	  if(differ > 20 and differ < 100) {
 	    
-	    // Adds the calculated tof to the vector tof
+	    // Adds the calculated TOF to the vector tof
 	    tof.insert(tof.end(), differ);
 
-	    // Adds the timestamp for each downstream hit to the vector timeToSpillDst
+	    // Adds the timestamp for each downstream hit to the vector timeStampDst
 	    // dst_wv.at(0)->TimeStamp() gives the TTT (Trigger Time Tag) since a spill
-	    //    each tick of the TTT is in 8 ns
-	    // Then add the downstream hit to that number to get out final number
-	    double theAnswer = (dst_wv.at(0)->TimeStamp()*8)+dstof_hits[dst_hit];
-	    timeToSpillDst.insert(timeToSpillDst.end(), theAnswer);
-
-	    // LArSoft Magic
-	    // my_tof_ptr->push_back(differ);
+	    //    each tick of the TTT is 8 ns
+	    // Then add the downstream hit to that number to get out final timetamp
+	    double time = (dst_wv.at(0)->TimeStamp()*8)+dstof_hits[dst_hit];
+	    timeStampDst.insert(timeStampDst.end(), time);
       
-	    // Fills debug histos with tof, timestamp, and hit time (time header)
+	    // Fills debug histos with tof, timestamp, and hit time for the ust hits
 	    tof_counts->Fill(differ);
-	    timestamp_histo->Fill(theAnswer);
+	    timestamp_histo->Fill(time);
 	    ustof_histo->Fill(ustof_hits[ust_hit]); // only ust_hits that matched with dst hits   
 
 	  }
 	}
       }
       
-      /*
-      for(size_t x = 0; x < tof.size(); ++x) { 
-	std::cout<<" TOF" << tof[x] << " ";
-	} */
+      // At this point, all the information for a single trigger are found
 
-      // At this point, we have the hits for a given trigger stored in tof
-      // Attempting to do some larsoft magic
-      ldp::TOF please(tof, timeToSpillDst);
-      (*TOFCol).push_back( please );
+      // Time For some LArSoft magic. 
+      // Creates the object, pushes it, and Associates it to the trigger.
+      ldp::TOF TOFObject(tof, timeStampDst);
+      (*TOFCol).push_back( TOFObject );
       util::CreateAssn(*this, e, *(TOFCol.get()), theTrigger, *(TriggerTOFAssn.get()));
     
     }
   }
 
+  // Move LArSoft magic to save the information into the final ROOT file 
   e.put(std::move(TriggerTOFAssn));
   e.put(std::move(TOFCol));
+
   return;
 }
 
+
 std::vector<short> lrm::TimeOfFlight::find_hits(std::vector<short> wv) {
+  // Hit finder for an inputted waveform
+
+  // The threshold for what qualifies for a hit
+  // Negative because we are looping for negative pulses
   float threshold = -40;
-  float gradient;
+
+  // Vector that will be storing all of the found hits
   std::vector<short> hits;
   
   bool rising_edge = false;
 
+  // Takes the gradient of the whole waveform
+  // Uses the Centered Difference Quotient
+  // Starts from 2 because the gradient can't be taken from the edges
   for(unsigned short i = 2; i < wv.size(); ++i) {
-    gradient = float(wv.at(i)-wv.at(i-2))/2;
     
+    float gradient = float(wv.at(i)-wv.at(i-2))/2;
+    
+    // Uses the rising_edge variable to test if a hit has already been found or not
     if(gradient < threshold && rising_edge == false) {
       hits.insert(hits.end(),i);
       rising_edge = true;
     }
-
+    
+    // Uses the negative threshold to indicate that we have left a hit
     if(gradient > abs(threshold) && rising_edge == true) {
       rising_edge = false;
     }
 
   }
   
+  // Gives a hit of 0 if nothing else was found to prevent seg faults
   if(hits.size() == 0) { hits.insert(hits.end(), 0); }
-
-  /* for(size_t h = 0; h < hits.size(); ++h) {
-     std::cout << hits[h] << ", ";
-     } */
-  
+ 
   return hits;
 
 }
 
 std::vector<short> lrm::TimeOfFlight::match_hits(std::vector<short> hits1, std::vector<short> hits2) {
+  // Matches the hits found on a single TOF paddle
 
-  short time_threshold = 10; // Nanoseconds
+  // The hits have to be within the threshold, measured in nanoseconds
+  short time_threshold = 10; 
 
   const size_t len_of_hits1 = hits1.size();
   const size_t len_of_hits2 = hits2.size();
@@ -246,12 +250,15 @@ std::vector<short> lrm::TimeOfFlight::match_hits(std::vector<short> hits1, std::
 
   std::vector<short> matched_hits;
 
-  // Creates a table of differences for hits1 on the x and hits 2 on the y
+  // Creates a table of differences, with  hits1 on the x and hits2 on the y
   for(size_t row = 0; row < len_of_hits1; row++) {
     for(size_t col = 0; col < len_of_hits2; col++) {
-      diff_array[row][col] = abs(int(hits1[row]-hits2[col])); //wary of int
+      diff_array[row][col] = abs(int(hits1[row]-hits2[col])); 
     }
   }
+
+  // Using the difference table, we find all of lowest time differences between 
+  //  hits found and, if that is in a given threshold, return that hit's TDC time
 
   for(size_t row = 0; row < len_of_hits1; row++) {
 
@@ -271,20 +278,18 @@ std::vector<short> lrm::TimeOfFlight::match_hits(std::vector<short> hits1, std::
     }
   }
   
-  /* for(size_t x = 0; x < matched_hits.size(); ++x) {
-     std::cout<<" matched hits " << matched_hits[x] << ", ";
-     } */
-
   return matched_hits;
 }
 
 void lrm::TimeOfFlight::beginJob()
 {
-  // Implementation of optional member function here.
+  // Opens up the file service to read information from the ROOT file input
   art::ServiceHandle<art::TFileService> tfs;
+  
+  // Setting up the debugging histograms
   tof_counts  = tfs->make<TH1F>("tof_counts" , "tof_counts" ,   100, 0.,   100.);
   timestamp_histo = tfs->make<TH1F>("timestamp_histo", "timestamp_histo", 10000, 0., 9000000000.);
-  ustof_histo = tfs->make<TH1F>("ustof_histo", "ustof_histo", 14000, 0., 14000.);
+  ustof_histo = tfs->make<TH1F>("ustof_histo", "ustof_histo", 10000, 0., 0.);
   tof_counts->GetXaxis()->SetTitle("ToF (ns)");
   tof_counts->GetYaxis()->SetTitle("N counts");
 }
@@ -316,16 +321,10 @@ void lrm::TimeOfFlight::endSubRun(art::SubRun & sr)
 
 void lrm::TimeOfFlight::reconfigure(fhicl::ParameterSet const & p)
 {
-
-   // Here lies some pseudo-code for making objects and their associations 
-   // (DON'T USE ME EXPLICITLY....FOR ILLUSTRATION ONLY)
-   // produces< std::vector<recob::WireChamberTrack>>(); 
-   // produces< art::Assns<raw::Trigger, recob::WireChamberTrack>>(); 
    
    // Implementing the ability to pass the name of the TriggerUtililty 
    fTriggerUtility   = p.get< std::string >("TriggerUtility", "FragmentToDigit");
 
-  // Implementation of optional member function here.
 }
 
 void lrm::TimeOfFlight::respondToCloseInputFile(art::FileBlock const & fb)
