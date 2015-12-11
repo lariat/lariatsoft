46e79bd73d3bfcf33e2f8f216071a7e2afd3f4a4
Brandon Soubasis <soubasis@lariatgpvm01.fnal.gov>
2015-12-11 11:56:22 -0600

    update

--- BeamlineSim/MergeFiles.sh
+++ BeamlineSim/MergeFiles.sh
@@ -1,2 +1,2 @@
-hadd sim_mergedroot_3m.root  /pnfs/lariat/scratch/users/$USER/sim_*.root
-rm /pnfs/lariat/scratch/users/$USER/sim_*.root
+hadd sim_mergedroot_3m.root  /pnfs/lariat/scratch/users/$USER/MCdata/sim_*.root
+rm /pnfs/lariat/scratch/users/$USER/MCdata/sim_*.root
