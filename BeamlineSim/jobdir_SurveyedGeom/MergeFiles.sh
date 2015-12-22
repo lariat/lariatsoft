a47c8d1bf9876f96c77f348a7b8525667815ebbd
Brandon Soubasis <soubasis@lariatgpvm01.fnal.gov>
2015-12-22 16:15:31 -0600

    Add Force removing

--- BeamlineSim/jobdir_SurveyedGeom/MergeFiles.sh
+++ BeamlineSim/jobdir_SurveyedGeom/MergeFiles.sh
@@ -1,2 +1,2 @@
 hadd sim_mergedroot_3m.root  /pnfs/lariat/scratch/users/$USER/MCdata/sim_*.root
-rm /pnfs/lariat/scratch/users/$USER/MCdata/sim_*.root
+rm -f /pnfs/lariat/scratch/users/$USER/MCdata/sim_*.root
