30854bec0b62c7f0aacd66d22ca98a3ed83cd662
Lynn Garren <garren@fnal.gov>
2016-02-16 14:52:00 -0600

    DetectorPropertiesService is unused in this context

--- LArIATRecoModule/GaussHitFinderT1034_module.cc
+++ LArIATRecoModule/GaussHitFinderT1034_module.cc
@@ -52,7 +52,7 @@
 #include "lardata/RecoBase/Wire.h"
 #include "lardata/RecoBase/Hit.h"
 //#include "RecoBaseArt/HitCreator.h"
-#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
+//#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
 #include "lardata/Utilities/AssociationUtil.h"
 
 // ROOT Includes
@@ -271,7 +271,7 @@ void GausHitFinder::produce(art::Event& evt)
    // ###################################
    // ### Calling Detector Properties ###
    // ###################################
-   auto const* detprop = lar::providerFrom<detinfo::DetectorPropertiesService>();
+   //auto const* detprop = lar::providerFrom<detinfo::DetectorPropertiesService>();
    
    // ################################
    // ### Calling Geometry service ###
