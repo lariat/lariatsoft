08368ed9e58aa280635fd9bdf851719583b59f5a
Greg Pulliam <gpulliam@lariatgpvm04.fnal.gov>
2018-04-23 17:28:24 -0500

    nvm, now the beamline simulation is working

--- BeamlineSim/jobdir_SurveyedGeom/Script.sh
+++ BeamlineSim/jobdir_SurveyedGeom/Script.sh
@@ -9,8 +9,8 @@ setup jobsub_client v1_2_6_2
 setup G4beamline v2_16a -q e6:prof:nu
 setup ifdhc v2_3_0 
 setup root v5_34_23 -q e6:prof
-jobsize=1000
-SUBspillcount=1
+jobsize=Size
+SUBspillcount=subspillcountn
 first=$((${PROCESS}*${jobsize}))
 last=$(( ${first} + $jobsize - 1 ))
 echo "PROCESS is: $PROCESS"
@@ -36,8 +36,8 @@ REALUSER=`basename ${X509_USER_PROXY} .proxy | grep -o -P '(?<=_).*(?=_)'`
 echo '$USER: ' $USER
 echo '$REALUSER: ' $REALUSER
 
-ifdh cp MergedAtStartLinesim_input.root /pnfs/lariat/scratch/users/$REALUSER/MCdatatest/MergedAtStartLinesim_input$SUBSPILL.root
-ifdh cp MergedAtStartLinesim_input.pickle /pnfs/lariat/scratch/users/$REALUSER/MCdatatest/MergedAtStartLinesim_input$SUBSPILL.pickle
+ifdh cp MergedAtStartLinesim_input.root /pnfs/lariat/scratch/users/$REALUSER/MCdata/MergedAtStartLinesim_input$SUBSPILL.root
+ifdh cp MergedAtStartLinesim_input.pickle /pnfs/lariat/scratch/users/$REALUSER/MCdata/MergedAtStartLinesim_input$SUBSPILL.pickle
 ls -lrth
 echo $CONDOR_DIR_INPUT
 
