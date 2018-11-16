562826c71fda9f476a8d65f1e5fa674af23ab13e
William Foreman <wforeman@uchicago.edu>
2018-11-16 14:20:58 -0600

    Don't filter hits by default (oops)

--- LArIATFilterModule/HitNumberFilter_module.cc
+++ LArIATFilterModule/HitNumberFilter_module.cc
@@ -107,7 +107,7 @@ bool HitNumberFilter::filter(art::Event & e)
   for(size_t i=0; i<2; i++){
   LOG_VERBATIM("HitNumberFilter")
   <<"#hits on plane "<<i<<": "<<nHits[i];
-    if( nHits[i] > fMaxNumHits[i] ) {
+    if( fMaxNumHits[i] > 0 && nHits[i] > fMaxNumHits[i] ) {
       pass = false;
       break;
     }
@@ -139,7 +139,7 @@ void HitNumberFilter::endJob()
 
 void HitNumberFilter::reconfigure(fhicl::ParameterSet const & p)
 {
-  fMaxNumHits	      = p.get< std::vector<int> > ("MaxNumHits",{1000,1000});
+  fMaxNumHits	      = p.get< std::vector<int> > ("MaxNumHits",{-999,-999});
   fHitsModuleLabel    = p.get< std::string >      ("HitsModuleLabel","gaushit");
   fHitsInstance       = p.get< std::string >      ("HitsInstance","");
 }
